a70cc9c8363cf13aa36e7ffd2eefa6a7ef38d1c0
parnavh <parnav100sach@gmail.com>
2024-10-24 13:54:53 +0530

    ui completed

--- src/components/option/option.svelte
+++ src/components/option/option.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
+  import { Label } from "@/components/ui/label";
+  import { Switch } from "@/components/ui/switch";
+  import * as Tooltip from "@/components/ui/tooltip";
+  import { CircleHelp } from "lucide-svelte";
 
-export let id: string;
-export let handler: () => void;
+  export let id: string;
+  export let handler: (id: string) => void;
+  export let checked: boolean = false;
+  export let tooltip: string = "";
 </script>
 
 <div class="px-2 flex justify-between">
-  <p><slot /></p>
-  <Switch on:click={() => handler(id)} />
+  <Label for={id} class="my-auto"><slot /></Label>
+  <div class="flex gap-2 items-center">
+    {#if tooltip !== ""}
+      <Tooltip.Root>
+        <Tooltip.Trigger>
+          <CircleHelp size={20} class="stroke-gray-400 dark:stroke-zinc-400" />
+        </Tooltip.Trigger>
+        <Tooltip.Content>
+          <p>{tooltip}</p>
+        </Tooltip.Content>
+      </Tooltip.Root>
+    {/if}
+    <Switch {id} on:click={() => handler(id)} {checked} />
+  </div>
 </div>
--- src/entrypoints/options/App.svelte
+++ src/entrypoints/options/App.svelte
@@ -1,27 +1,63 @@
-<script>
-import * as Tabs from "@/components/ui/tabs";
-import { Option } from "@/components/option";
+<script lang="ts">
+  import * as Tabs from "@/components/ui/tabs";
+  import { Option } from "@/components/option";
 
-function handler(id) {
-  console.log(id);
-}
+  function handler(id: string) {
+    console.log(id);
+  }
+
+  const options = {
+    general: [
+      {
+        id: "banner",
+        tooltip:
+          "Automatically dismisses a banner after you dismiss it for the first time!",
+        text: "Automatically remove banner",
+      },
+      {
+        id: "playback-speed",
+        text: "Persist video playback speed",
+        tooltip: "",
+      },
+    ],
+    gregmat: [
+      {
+        id: "vocab-mountain",
+        text: "Disable vocab mountain keybinds",
+        tooltip: "Includes gre and toefl vocab mountain",
+      },
+    ],
+    prepswift: [
+      {
+        id: "prepswift-stats",
+        text: "Show prepswift completion stats",
+        tooltip: "Shows total time and lectures completed",
+      },
+    ],
+  } as const;
 </script>
 
 <main class="h-lvh dark:bg-black dark:text-white">
   <div class="h-lvh grid place-items-center">
-    <Tabs.Root value="general" class="w-[300px]">
-      <Tabs.List class="w-[300px]">
+    <Tabs.Root value="general" class="w-[400px]">
+      <Tabs.List class="w-[400px]">
         <Tabs.Trigger value="general" class="w-full">General</Tabs.Trigger>
         <Tabs.Trigger value="gregmat" class="w-full">GregMat</Tabs.Trigger>
         <Tabs.Trigger value="prepswift" class="w-full">PrepSwift</Tabs.Trigger>
       </Tabs.List>
-      <Tabs.Content value="general">
-        <div class="flex flex-col gap-2">
-          <Option id="hello" {handler}>hello</Option>
-        </div>
-      </Tabs.Content>
-      <Tabs.Content value="gregmat">greg</Tabs.Content>
-      <Tabs.Content value="prepswift">prep</Tabs.Content>
+      {#each Object.entries(options) as [cat, val]}
+        <Tabs.Content value={cat}>
+          <div
+            class="flex flex-col gap-2 font-mono text-gray-800 dark:text-gray-200"
+          >
+            {#each val as option}
+              <Option id={option.id} {handler} tooltip={option.tooltip}
+                >{option.text}</Option
+              >
+            {/each}
+          </div>
+        </Tabs.Content>
+      {/each}
     </Tabs.Root>
   </div>
 </main>
